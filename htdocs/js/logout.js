51b8d262b1bbe93539c1bba19ee7453a6313cbb5
Carloprime8 <67376092+Carloprime8@users.noreply.github.com>
2023-06-27 18:55:37 +0800

    new

--- htdocs/js/logout.js
+++ htdocs/js/logout.js
@@ -1,25 +1,27 @@
-// Function to send a POST request to the logout endpoint
-async function logout() {
-    try {
-        const response = await fetch('/api/logout', {
-            method: 'GET',
-            credentials: 'same-origin', // Include cookies in the request
-        });
+// Function to send a GET request to the logout endpoint
+function logout() {
+    fetch('/api/logout', {
+      method: 'GET',
+      credentials: 'same-origin' // Include cookies in the request
+    })
+      .then(response => {
         if (response.ok) {
-            // Redirect to the login page after successful logout
-            window.location.href = '/login';
+          // Redirect to the login page after successful logout
+          window.location.href = '/login';
         } else {
-            const errorMessage = await response.text();
-            console.error(errorMessage);
-            // Display an error message to the user
-            alert('Logout failed');
+          return response.text().then(errorMessage => {
+            throw new Error(errorMessage);
+          });
         }
-    } catch (error) {
-        console.error(error);
+      })
+      .catch(error => {
+        console.error('Logout failed:', error);
         // Display an error message to the user
         alert('Logout failed');
-    }
-}
-// Add event listener to the logout button
-const logoutButton = document.getElementById('logoutButton');
-logoutButton.addEventListener('click', logout);
+      });
+  }
+  
+  // Add event listener to the logout button
+  const logoutButton = document.getElementById('logoutButton');
+  logoutButton.addEventListener('click', logout);
+  
